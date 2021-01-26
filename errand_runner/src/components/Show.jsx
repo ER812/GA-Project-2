c92ddff1cbb205992cecacc2fa9d95445e63ab45
Elizabeth <Elizabeth.Rights812@gmail.com>
2021-01-26 16:57:35 -0500

    updated readme

--- errand_runner/src/components/Show.jsx
+++ errand_runner/src/components/Show.jsx
@@ -7,13 +7,20 @@ function Show(props) {
     return errand.id === params.id
   })
 
+  if (!errand) {
+    return (
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    )
+  }
 
   return (
     <div className="showText">
-      <h1>Location:{errand.fields.organization}</h1>
-      <h2>Errand:{errand.fields.errand}</h2>
-      <h2>Notes:{errand.fields.notes}</h2>
-      <h2>Payments Accepted:{errand.fields.paymentsAccepted}</h2>
+      <h1>Location: {errand.fields.organization}</h1>
+      <h2>Errand: {errand.fields.errand}</h2>
+      <h2>Notes: {errand.fields.notes}</h2>
+      <h2>Payments Accepted: {errand.fields.paymentsAccepted.join(", ")}</h2>
       {/* <h2>{errand.fields.openingHours}</h2> */}
       {/* <h2>{errand.fields.closingHours}</h2> */}
       
